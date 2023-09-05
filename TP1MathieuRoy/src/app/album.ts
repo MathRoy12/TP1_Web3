db15ac1028a5ea46e76753b2f5c233c204414f0a
Mathieu Roy <2234476@cegepmontpetit.ca>
2023-09-05 09:49:23 -0400

    FCT page principale
    design de la page principale plus l'ajout de l'apparition des image

--- TP1MathieuRoy/src/app/album.ts
+++ TP1MathieuRoy/src/app/album.ts
@@ -2,7 +2,6 @@ import {Chanson} from "./chanson";
 
 export class Album {
   constructor(public name: String,
-              public artist: String,
-              public lstChanson: Chanson[]) {
+              public img :String) {
   }
 }
--- TP1MathieuRoy/src/app/app.component.ts
+++ TP1MathieuRoy/src/app/app.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Chanson} from './chanson';
+import {Album} from "./album";
+import {HttpClient} from '@angular/common/http';
+import {lastValueFrom} from "rxjs";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'TP1MathieuRoy';
+export class AppComponent implements OnInit {
+  artist: string = '';
+  lstAlbum: Album[] = [];
+  lstChanson: Chanson[] = [];
+
+  constructor(public http: HttpClient) {
+  }
+
+  async ngOnInit(): Promise<void> {
+    let rep1 = await lastValueFrom(this.http.get<any>('https://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=Megadeth&api_key=9a8a3facebbccaf363bb9fd68fa37abf&format=json'))
+    this.artist = rep1.artist.name
+    let rep2 = await lastValueFrom(this.http.get<any>('https://ws.audioscrobbler.com/2.0/?method=artist.gettopalbums&artist=Megadeth&api_key=9a8a3facebbccaf363bb9fd68fa37abf&format=json'))
+
+    for (let item of rep2.topalbums.album) {
+      let titre: String = '';
+      let img: String = '';
+
+      titre = item.name;
+      img = item.image[2]["#text"];
+
+      this.lstAlbum.push(new Album(titre, img))
+    }
+  }
+
+
+
 }
